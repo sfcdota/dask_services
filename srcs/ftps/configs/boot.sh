a70a03fc5baf4180e0d5d9b3d91d5a0bccc6eef0
sfcdota <sfcdota@gmail.com>
2021-04-23 22:51:30 +0300

    sdf

--- srcs/ftps/configs/boot.sh
+++ srcs/ftps/configs/boot.sh
@@ -1,3 +1,3 @@
 echo "$FTPS_USERNAME:$FTPS_PASSWORD" | chpasswd
-adduser -h /root/csv-files "test"
+adduser -h /root/csv-files "test" -s /bin/shells
 echo "$FTPS_TEST_USERNAME:$FTPS_TEST_PASSWORD" | chpasswd
